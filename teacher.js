66e4d3d0c9262faa8a985ea7ff5fc4f48d0c706d
Steven Kitscha <stevenkitscha@Stevens-MacBook-Air.local>
2019-03-10 20:23:49 -0500

    Get Operations Merge

--- teacher.js
+++ teacher.js
@@ -5,7 +5,7 @@ const config = require('./config/config.js');
      module.exports.handler = vandium.api()
         .GET( (event, context, callback) => {
           let index = event.queryStringParameters.index;
-          var selectQuery='SELECT * FROM  nextdoormilwaukeedb.Students s';
+          var selectQuery='SELECT * FROM  nextdoormilwaukeedb.Teachers t';
         var queryStringParams=Object.keys(event.queryStringParameters);
         var firstParam=true;
         if (queryStringParams.length!=0){
@@ -19,31 +19,30 @@ const config = require('./config/config.js');
                          selectQuery+=` and`;
                     }
                     if (query !='fullName'){
-                    selectQuery+=` s.${query}='${event.queryStringParameters[query]}'`
+                    selectQuery+=` t.${query}='${event.queryStringParameters[query]}'`
                     }
                     else{
-                         selectQuery+=` s.${query} like '%${event.queryStringParameters[query]}%'` 
+                         selectQuery+=` t.${query} like '%${event.queryStringParameters[query]}%'` 
                     }
                 }
                 })
-                    
         }
+
         if(index!=null){
             selectQuery+=` limit ${index},25;`
         }
- 
         var connection = mysql.createConnection(config.databaseoptions);
-        connection.query(selectQuery, function (error, results, fields) { 
-         if (error) { callback(null,error) }
-         callback( null,
-            {
-                 "statusCode": 200,
-                 "headers": { 
-                     "Access-Control-Allow-Origin": "*"
-                 },
-                 "body":results
-            });
-        });
+	      connection.query(selectQuery, function (error, results, fields) { 
+            if (error) { callback(null,error) }
+            callback( null,
+               {
+                    "statusCode": 200,
+                    "headers": { 
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    "body":results
+               });
+	      });
         connection.end();
             
         })
